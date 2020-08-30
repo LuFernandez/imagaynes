108d68575507fcefc93da256b986ef699e9f8783
LuFernandez <lu.gfernandez@gmail.com>
2020-08-30 16:04:06 -0300

    subi cosas para el informe

---
+++ analisis_scripts/scripts/ex1.m
@@ -0,0 +1,24 @@
+clear all;
+close all;
+
+h = fspecial('unsharp');             %% Hi pass
+
+freqz2(h);
+
+% construyo una imagen (que es una delta en 0)
+% suficientemente grande para poder ver bien el espectro
+
+N=256;
+big=zeros(N);       %make a big image
+big(N/2,N/2)=1;     %unit impulse
+
+h1=conv2(big,h);    %make conv with the filter ==> 
+
+figure
+S = fft2(h1);       %Spectrum
+SM=abs(S);          %Modulo
+imshow(fftshift(SM/max(max(SM)))); % show spect
+
+figure
+IMd = log(1+abs(SM));
+imshow(fftshift(IMd/max(max(IMd)))); % show spect log scale
---
+++ analisis_scripts/scripts/ex2.m
@@ -0,0 +1,16 @@
+clear all;
+close all;
+h=[0 1/6 0 ; 1/6 1/3 1/6 ; 0 1/6 0 ];
+
+h = h/sum(h(:));
+h = fspecial('unsharp');
+
+b=imread('barbara.gif'); 
+imshow(b);
+
+h1=imfilter(b,h);    %make conv with the filter ==> 
+
+figure
+imshow(h1);
+
+
